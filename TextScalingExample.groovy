610224d935bab1adb65cde00eef1551cfb29ca70
Kevin Harrington <kharrington@bancroftschool.org>
2024-02-29 12:05:15 -0500

    adding code

--- TextScalingExample.groovy
+++ TextScalingExample.groovy
@@ -1,1 +1,14 @@
 // code here
+
+CSG textToSize(String text, double x, double y double z) {
+		CSG startText = CSG.text( text, z) ;
+		double scalex = x/startText.getTotalX();
+		double scaley = y/startText.getTotalY();
+		return startText
+				.scalex(xscale)
+				.scaley(yScale)
+				.toXMin()
+				.toYMin();
+	}
+
+return textToSize("Testing", 30,20,10)
